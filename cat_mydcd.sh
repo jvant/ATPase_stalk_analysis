1cbfb8f192e4ca5bd0edd1803800d98012d7fe44
jvant <jvant@asu.edu>
2020-02-07 11:44:24 -0700

    cleaned up the directory

--- cat_mydcd.sh
+++ cat_mydcd.sh
@@ -16,12 +16,32 @@
 # Notes: 
 # 
 ############################################################## 
-
-# VMD make index file
-vmd -dispdev text -e ../make_index.tcl -args ${1} ${2}
-
+# Paths
 catdcd="/home/jvant/Documents/Programs/catdcd4.0/catdcd"
 
-dcds=$(find ./ -type f -name "step*.dcd")
+############################################################## 
+if [ -e $catdcd ]
+then
+    echo "catdcd is not where you think it is!"
+    echo "Path is set as:"
+    echo "$catdcd"
+    echo "would you like to provide a path to catdcd? [y/n]"
+    read ans
+    if [ "y" == $ans ]
+    then
+	echo "What is the path?  end with catdcd executable."
+	read catdcd
+    else
+	echo "Ok we are done here."
+	exit
+    fi
+fi
+
+# VMD make index file
+vmd -dispdev text -e ../Modulus_Scripts/make_index.tcl -args ${1} ${2}
+dcds=$(fqind ./ -type f -name "step*.dcd")
 
 $catdcd -o wo_water_all.dcd -i my.ind $dcds 
+
+echo "Congrats!  We are done :}"
+
