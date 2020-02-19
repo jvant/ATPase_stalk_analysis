356a95194fecc6885046f1b65c9d76635d29da1e
John Vant <jvant@asu.edu>
2020-02-19 01:16:06 -0800

    Release

--- ModMaster.tcl
+++ ModMaster.tcl
@@ -1,26 +1,23 @@
 ##################################################################################  
 #                                                                                   
-#                    Authors: Abhishek Singharoy, John Vant                         
-#                              Date: 01/19/19                                       
+#                    Authors: John Vant, Abhishek Singharoy
+#                              Date: 02/18/20
 #                        Center for Structural Discovery                            
 #                           Biodesign Institute, ASU                                
 #                                                                                   
 ##################################################################################  
 #                                                                                   
 #                             Purpose:                                              
-# To Determine the angular fluctuations using orient package                        
-#  Verision 2.2 uses origonal principle vectors to calculate                        
+# To determine the angular fluctuations of ATPase coiled-coil via principleaxis 
+# assignment
+#  
 #                                                                                   
 ##################################################################################  
-# Usage: vmd -dispdev text -e ModMaster.tcl -args 
+# Usage: vmd -dispdev text -e ModMaster.tcl -args [-psf <filename>] [-dcd <filename>] 
 ##################################################################################  
 #                          Import packages               
-#
+
 package require Orient
-source /usr/share/tcl8.5/tcllib-1.14/math/math.tcl
-source /usr/share/tcl8.5/tcllib-1.14/math/statistics.tcl
-source /usr/share/tcl8.5/tcllib-1.14/math/linalg.tcl 
-package require math::statistics
 namespace import Orient::orient
 
 ##################################################################################  
@@ -29,29 +26,30 @@ namespace import Orient::orient
 # Read in Arguments
 # Set the defaults
 array set options {-psf "my.psf"}
+array set options {-dcd "wo_water_all.dcd"}
 # Read in the arguments
 array set options $argv
 # Use them
 source ./params
 puts "Your PSF file is: $options(-psf)"
-
+puts "Your DCD file is: $options(-dcd)"
+puts " "
 # Move to working directory
 puts "Working out of the following directory"
 puts [pwd]
 
 # load psf
-mol new my.psf
+mol new $options(-psf)
 #set dcds [glob *dcd]
 set dcds "wo_water_all.dcd"
-foreach name $dcds {
+foreach name $options(-dcd) {
     puts "The dcd file is..."
     puts $name
     mol addfile $name waitfor -1
 }
-puts "did it load"
+
 # Get total number of frames                   
 set nf [molinfo top get numframes]
-puts "These trajectories amount to [expr $nf / 1000] ps"
 
 ##################################################################################  
 # Torsional Modulus Calculations
@@ -59,21 +57,18 @@ puts "These trajectories amount to [expr $nf / 1000] ps"
 # Local variables                              
 set convert [expr 180/3.14]
 
-################
 #### New Make atomselections
 source ./selections
 puts $selections(1)
 
 for {set i 1} {$i <= [array size selections]} {incr i} {
-    set "Pt($i)" [atomselect top $selections($i)] ; list
+    set "Pt($i)" [atomselect top $selections($i)] ;
 }
 for {set i 1} {$i <= [array size selections]} {incr i} {
-    set "Pto($i)" [atomselect top $selections($i) frame 0] ; list
+    set "Pto($i)" [atomselect top $selections($i) frame 0] ;
 }
 
 
-
-################
 # Open .dat files for writing
 for {set i 1} {$i <= [array size selections]} {incr i} {
     set "outfilea($i)" [open "Pta$i.dat" w];
@@ -84,10 +79,10 @@ for {set i 1} {$i <= [array size selections]} {incr i} {
 # Define principal axes for reference frame 0
 echo "vecor math"
 for {set i 1} {$i <= [array size selections] } {incr i} {
-    set I [draw principalaxes $Pto($i)] ; list
-    set "VPa0($i)" [lindex $I 0] ; list
-    set "VPb0($i)" [lindex $I 1] ; list
-    set "VPc0($i)" [lindex $I 2] ; list
+    set I [draw principalaxes $Pto($i)] ;
+    set "VPa0($i)" [lindex $I 0] ;
+    set "VPb0($i)" [lindex $I 1] ;
+    set "VPc0($i)" [lindex $I 2] ;
 }
 
 puts "align protein at frame i to frame 0"
@@ -96,7 +91,7 @@ set sel0 [atomselect top "protein" frame 0]
 
 # Start for loop for setting vectors of all other frames
 for {set i 1 } {$i < $nf } { incr i } {
-    set sel [atomselect top "protein" frame $i] ; list
+    set sel [atomselect top "protein" frame $i] ; 
     $sel move [measure fit $sel $sel0]
 
 # obtaining principle components
@@ -104,29 +99,29 @@ puts "insigia principle components"
     for {set l 1} {$l <= [array size selections] } {incr l} {
 	$Pt($l) frame $i
 	$Pt($l) num
-	set I [draw principalaxes $Pt($l)] ; list
-	set "VPa($l)" [lindex $I 0] ; list
-	set "VPb($l)" [lindex $I 1] ; list
-	set "VPc($l)" [lindex $I 2] ; list
+	set I [draw principalaxes $Pt($l)] ; 
+	set "VPa($l)" [lindex $I 0] ; 
+	set "VPb($l)" [lindex $I 1] ; 
+	set "VPc($l)" [lindex $I 2] ; 
 }
 
 # combinations  
 echo "combinations"
     for {set l 1} {$l <= [array size selections] } {incr l} {
-	set anglea($l) [expr $convert*acos( [vecdot $VPa0($l) $VPa($l)] ) ]; list
-	set angleb($l) [expr $convert*acos( [vecdot $VPb0($l) $VPb($l)] ) ] ; list
-	set anglec($l) [expr $convert*acos( [vecdot $VPc0($l) $VPc($l)] ) ] ; list
+	set anglea($l) [expr $convert*acos( [vecdot $VPa0($l) $VPa($l)] ) ]; 
+	set angleb($l) [expr $convert*acos( [vecdot $VPb0($l) $VPb($l)] ) ] ; 
+	set anglec($l) [expr $convert*acos( [vecdot $VPc0($l) $VPc($l)] ) ] ;
 
       if {$anglea($l) > 90} {
        set anglea($l) [expr (180 - $anglea($l))]
       }
 	puts $outfilea($l) $anglea($l) 	
      if {$angleb($l) > 90} {
-       set angleb($l) [expr (180 - $angleb($l))] ; list
+       set angleb($l) [expr (180 - $angleb($l))] ;
       }
 	puts $outfileb($l) $angleb($l)
      if {$anglec($l) > 90} {
-       set anglec($l) [expr (180 - $anglec($l))] ; list
+       set anglec($l) [expr (180 - $anglec($l))] ;
       }
 	puts $outfilec($l) $anglec($l)
     }
@@ -138,111 +133,5 @@ for {set i 1} {$i <= [array size selections] } {incr i} {
     close $outfileb($i) 
     close $outfilec($i) 
 }
-
-# Calc Standard deviation and Kmod
-puts "reading in data file"
-
-set k 1.38064852e-23
-set temp 300
-set outfilekmod [open "allkmods.dat" w]
-puts $outfilekmod "tors_kmod_a tors_kmod_b tors_kmod_c" 
-for {set i 1} {$i <= [array size selections] } {incr i} {
-    set infilea($i) [open "Pta$i.dat" r]
-    set infileb($i) [open "Ptb$i.dat" r]
-    set infilec($i) [open "Ptc$i.dat" r]
-    set data_a($i) [read $infilea($i)]
-    set data_b($i) [read $infileb($i)]
-    set data_c($i) [read $infilec($i)]
-    set std_a($i) [::math::statistics::stdev $data_a($i)]
-    set std_b($i) [::math::statistics::stdev $data_b($i)]
-    set std_c($i) [::math::statistics::stdev $data_c($i)]
-    set tors_kmod_a [expr [expr [expr $k * $temp] / [expr pow($std_a($i),2)] ] * pow(10,21)]
-    set tors_kmod_b [expr [expr [expr $k * $temp] / [expr pow($std_b($i),2)] ] * pow(10,21)]
-    set tors_kmod_c [expr [expr [expr $k * $temp] / [expr pow($std_c($i),2)] ] * pow(10,21)]
-    
-    puts $outfilekmod "$tors_kmod_a $tors_kmod_b $tors_kmod_c" 
-
-}
-close $outfilekmod
-
-
-##################################################################################  
-# Stretching Mod Calcs
-set "outfile" [open "Dist.dat" w];
-
-set sel_top [atomselect top "backbone and (segname PROG and not resid 101 to 287 and (z > 137 and z < 151 ))"]
-set sel_bottom [atomselect top "backbone and (segname PROG and not resid 101 to 287 and (z > 39 and z < 53 ))"]
-
-for {set i 1 } {$i < $nf } { incr i } {
-    $sel_top frame $i
-    $sel_bottom frame $i
-    set top_coord [measure center $sel_top weight mass]
-    set bottom_coord [measure center $sel_bottom weight mass]
-    set "dist($i)" [veclength [vecsub $top_coord $bottom_coord]]
-    puts $outfile $dist($i)
-}
-
-close $outfile
-
-##################################################################################  
-# Bending Mod Calcs
-
-# Define Sections
-## z cutpoint
-set center_point 95
-# Make atomselections
-set top [atomselect top "backbone and segname PROG and ( z > $center_point )"]
-set bottom [atomselect top "backbone and segname PROG and ( z < $center_point )"]
-
-# Open file for writing
-set "outfile" [open "Bend.dat" w];
-
-# Calculations
-
-for {set i 1 } {$i < $nf } { incr i } {
-    $top frame $i
-    $bottom frame $i
-    set I_top [draw principalaxes $top]
-    set I_bottom [draw principalaxes $bottom]
-    set "Vtop" [lindex $I_top 2]
-    set "Vbottom" [lindex $I_bottom 2]
-    set angle "[expr $convert*acos([vecdot $Vtop $Vbottom])]" 
-    if {$angle > 90} {
-       set angle [expr (180 - $angle)]
-      }
-    puts $outfile $angle 	
-}    
-# Close .dat file so it can be written
-close $outfile
-
-##################################################################################  
-# Helix only Bending Mod Calcs
-
-# Define Sections
-# Make atomselections
-set top [atomselect top "backbone and segname PROG and not resid 101 to 287 and ( z > $center_point )"]
-set bottom [atomselect top "backbone and segname PROG and not resid 101 to 287 and ( z < $center_point )"]
-
-# Open file for writing
-set "outfile" [open "Bend-wo-helix.dat" w];
-
-# Calculations
-
-for {set i 1 } {$i < $nf } { incr i } {
-    $top frame $i
-    $bottom frame $i
-    set I_top [draw principalaxes $top]
-    set I_bottom [draw principalaxes $bottom]
-    set "Vtop" [lindex $I_top 2]
-    set "Vbottom" [lindex $I_bottom 2]
-    set angle "[expr $convert*acos([vecdot $Vtop $Vbottom])]" 
-    if {$angle > 90} {
-       set angle [expr (180 - $angle)]
-      }
-    puts $outfile $angle 	
-}    
-# Close .dat file so it can be written
-close $outfile
-
 
 exit
--- pca.py
+++ pca.py
@@ -1,32 +1,51 @@
-#!/python
+#!/usr/bin/env python
 
-print('is this working?')
-# Load libraries
+############################################################## 
+# 
+# Author:               John Vant 
+# Email:              jvant@asu.edu 
+# Affiliation:   ASU Biodesign Institute 
+# Date Created:          200207
+# 
+############################################################## 
+# 
+# Usage: 
+# 
+############################################################## 
+# 
+# Notes: 
+# 
+############################################################## 
+
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
 from sklearn.decomposition import PCA
-from sklearn import datasets
+from scipy.constants import *
 
-# Load the data
-digits = datasets.load_digits()
-print(type(digits))
-digitsa = np.loadtxt('Pta1.dat')
-digitsb = np.loadtxt('Ptb1.dat')
-digitsc = np.loadtxt('Ptc1.dat')
-digits = [digitsa, digitsb, digitsc]
-print(digits)
+############################################################## 
+# Def Fun
+def kmodulus(std):
+    temp = 300
+    return k * temp/pow(std,2) * 10**21 # Gives values in pNnm
 
-# Standardize the feature matrix
-X = StandardScaler().fit_transform(digits.data)
-
-# Create a PCA that will retain 99% of the variance
-pca = PCA(n_components=0.99, whiten=True)
-
-# Conduct PCA
-X_pca = pca.fit_transform(X)
-
-# Show results
-print('Original number of features:', X.shape[1])
-print('Reduced number of features:', X_pca.shape[1])
-
-exit()
+############################################################## 
+# Main
+outdat = []
+for i in range(1,10):
+    X = np.array([np.loadtxt('Pta%s.dat' % i),
+                  np.loadtxt('Ptb%s.dat' % i),
+                  np.loadtxt('Ptc%s.dat' % i)])
+    X = X.T
+    
+    pca = PCA(n_components=1)
+    pca.fit(X)
+    X_pca = pca.transform(X)
+    kmod = kmodulus(np.std(X_pca))
+    print("original shape:   ", X.shape)
+    print("transformed shape:", X_pca.shape)
+    print("Explained var ratio",pca.explained_variance_ratio_)
+    print(kmod)
+    outdat.append(kmod)
+    
+np.savetxt('PCA.dat', outdat)
